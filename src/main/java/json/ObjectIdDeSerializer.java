dbc31174acc0e4ad6f7563dfa14ad1a3cd6bae06
Lukas Eichler <leichler@maredit.de>
2014-11-05 23:05:11 +0100

    Added copyright and license

--- src/main/java/json/ObjectIdDeSerializer.java
+++ src/main/java/json/ObjectIdDeSerializer.java
@@ -1,7 +1,22 @@
+/*
+ * Copyright (C) 2014 Lukas Eichler
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package json;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
@@ -15,7 +30,7 @@ import java.io.IOException;
  */
 public class ObjectIdDeSerializer extends JsonDeserializer<ObjectId>{
     @Override
-    public ObjectId deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+    public ObjectId deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
         ObjectCodec oc = jp.getCodec();
         JsonNode node = oc.readTree(jp);
         return new ObjectId(node.asText());
